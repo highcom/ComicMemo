8ef09d98ba1bcb00182d59bc09ae60c57acd495c
koichi highcom <youhappywith@yahoo.co.jp>
2023-07-03 23:27:53 +0900

    HiltモジュールでのDatabase生成処理にmigration設定が漏れていたので修正

--- app/src/main/java/com/highcom/comicmemo/di/ComicMemoModule.kt
+++ app/src/main/java/com/highcom/comicmemo/di/ComicMemoModule.kt
@@ -3,6 +3,7 @@ package com.highcom.comicmemo.di
 import android.content.Context
 import androidx.room.Room
 import com.highcom.comicmemo.datamodel.ComicMemoRoomDatabase
+import com.highcom.comicmemo.datamodel.MIGRATION_2_3
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,7 +19,7 @@ class ComicMemoModule {
     @Provides
     fun provideDatabase(
         @ApplicationContext context: Context
-    ) = Room.databaseBuilder(context, ComicMemoRoomDatabase::class.java, "ComicMemoDB").build()
+    ) = Room.databaseBuilder(context, ComicMemoRoomDatabase::class.java, "ComicMemoDB").addMigrations(MIGRATION_2_3).build()
 
     @Singleton
     @Provides
